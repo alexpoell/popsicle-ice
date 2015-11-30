20ced6b4386a48654a7a52aa7e7d285031cc32a7
Owen Williams <hi@owened.co.nz>
2015-11-30 09:39:12 -0800

    Subscribe page

---
+++ templates/page-subscribe.php
@@ -0,0 +1,53 @@
+<?php
+/*
+Template Name: Subscribe page
+*/
+?>
+</div></div>
+<div class="holder">
+
+      <div class="row">
+        <div class="col-lg-8 col-lg-offset-2 lead col-md-8 col-md-offset-2 col-sm-8 col-sm-offset-2">
+          <h1>WELCOME TO DESIGN LIFE</h1>
+          <p>A podcast about design and side projects for motivated creators</p>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-lg-8 col-lg-offset-2 col-md-8 col-md-offset-2 convert">
+          <h3>LAUNCHING NOVEMBER 30, 2015</h3>
+          <p>Join Charli and Femke as they chat about their obsession with creating, what keeps them motivated and how they balance projects on top of full time work. Want to be notified when our show drops? Sign up below and we’ll email you with each new episode.</p>
+
+          <div class="row">
+          <form action="http://designlife.us12.list-manage1.com/subscribe/post" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" novalidate="">
+                <input type="hidden" name="u" value="21bda0167c84a2842d8917290">
+                <input type="hidden" name="id" value="d364485418">
+                <div class="mobile-fix col-lg-8 col-xs-12 col-sm-12 form" style="padding-left:0px;"><input type="text" class="form-control" id="email" placeholder="EMAIL ADDRESS" name="EMAIL" id="mce-EMAIL"/></div>
+                <div class="mobile-fix col-lg-4 col-xs-12 col-sm-12 form" style="padding-right:0px;"><button class="button">Sign me up!</button></div>
+            </form>
+          </div>
+        </div>
+      </div>
+      <div class="row bump">
+        <div class="col-lg-4 col-lg-offset-2 col-md-4 col-md-offset-2 col-sm-6 left">
+          <div class="creators ">
+            <img src="/images/charli.png">
+             <p>CHARLI MARIE | <a href="http://twitter.com/charliprangley">@charliprangley</a></p>
+             <p>Living in London, Charli works full time as a marketing designer at retail analytics startup, <a href="http://edited.com/">EDITED</a>. When she’s not busy designing websites and swag, she’s <a href="https://www.youtube.com/user/charlimarieTV">editing videos</a>, <a href="http://linernotekids.com">selling her own t-shirts</a> or <a href="http://charlimarie.com/">writing honest thoughts on her blog.</a></p>
+          </div>
+        </div>
+        <div class="col-lg-4 col-md-4 col-sm-6 right">
+          <div class="creators ">
+            <img src="/images/fem.png">
+            <p>FEMKE VS | <a href="http://twitter.com/femkesvs">@femkesvs</a></p>
+            <p>From the canals of Amsterdam, Femke works by day in marketing and communications at <a href="http://atomic.io">Atomic</a> and by night as a <a href="http://theapartment.co">digital design freelancer</a>. Otherwise Femke is either catching up with other designers <a href="http://theapartment.co/blog/">on her blog</a> or <a href="https://instagram.com/apartmentdigital/">taking pictures.</a></p>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-lg-2 col-lg-offset-5 center">
+          <a target="_blank" href="https://twitter.com/intent/tweet?url=http%3A%2F%2Fdesignlife.fm&text=Excited%20for%20@charliprangley%20and%20@femkesvs%27%20podcast%20DESIGN%20LIFE%20-%20coming%20Nov%2030th%21%20@DesignLifeFM&
+"><button class="invert-button">Share this!</button></a>
+        </div>
+      </div>
+
+  </div>
