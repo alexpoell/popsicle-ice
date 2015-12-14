a2c54cd4ea700ad23adc17f8559bf5ec43350a39
Owen Williams <hi@owened.co.nz>
2015-12-14 20:49:48 +0100

    The feeling when you forget the responsive nav button

--- templates/header.php
+++ templates/header.php
@@ -2,6 +2,12 @@
   <div class="navbar navbar-default">
     <div class="container">
       <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse" aria-expanded="false" aria-controls="navbar">
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+        </button>
         <a class="brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
         <p class="slogan"><?php echo $bloginfo = get_bloginfo ( 'description' ); ?></p>
       </div>
