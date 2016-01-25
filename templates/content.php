ff448ad59b879889dc45564ed69cc98a24f10862
Owen Williams <hi@owened.co.nz>
2016-01-25 07:00:02 +0100

    Fix links and podcast player on homepage. Also increment font size for iPad/iPhone

--- templates/content.php
+++ templates/content.php
@@ -6,28 +6,23 @@
     }
     ?>
   </div>
-      <div class="col-lg-10 col-sm-12 col-xs-12">
-        <div
-   class="">
+      <div class="col-lg-10 col-sm-12 col-xs-12 podcast-holder">
     <article <?php //post_class(); ?>>
       <header>
         <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
         <?php get_template_part('templates/entry-meta'); ?>
       </header>
       <div class="entry-summary">
-        <?php the_field('post_description'); ?>
+        <p><?php the_field('post_description'); ?></p>
         <p class="read-more"><a href="<?php the_permalink(); ?>">Read the show notes ></a></p>
       </div>
 
 
     </article>
-  </div>
-  </div>
-
-
-</div>
-<div class="col-lg-10 col-lg-offset-1 podcast-player">
- <?php $podcast_content = get_the_powerpress_content(); ?>
+  <?php $podcast_content = get_the_powerpress_content(); ?>
 
   <?php the_powerpress_content(); ?>
+  </div>
+
+
 </div>
